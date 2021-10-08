251d069f1f41ed07654845c758dc4493c183b2df
0ngezwa-felele <ongezwa@codex.com>
2021-10-08 12:26:55 +0200

    test file modified

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -9,7 +9,7 @@ if (process.env.DATABASE_URL && !local) {
     useSSL = true;
 }
 // DB connection string
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/greeting';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/travis_ci_test';
 
 const pool = new Pool({
     connectionString
